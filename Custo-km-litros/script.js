65a6e765c149f9034d23da0d4e3f8b0c85529131
Evandro Ramos <164125280+evandrosilvaramos@users.noreply.github.com>
2026-01-17 15:18:18 -0300

    atualização

---
+++ Custo-km-litros/script.js
@@ -0,0 +1,18 @@
+document.getElementById('calcForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const precoLitro = parseFloat(document.getElementById('precoLitro').value);
+    const consumo = parseFloat(document.getElementById('consumo').value);
+    
+    if (precoLitro <= 0 || consumo <= 0) {
+        document.getElementById('resultado').innerHTML = 'Por favor, insira valores válidos.';
+        return;
+    }
+    
+    const custoPorKm = precoLitro / consumo;
+    const valorMinimoCorrida = (custoPorKm * 2) + 0.95;
+    const valorOtimo = (custoPorKm * 2) + 1.50;
+    const resultado = `Custo por km: R$ ${custoPorKm.toFixed(2)}<br>Valor mínimo para aceitar corrida: R$ ${valorMinimoCorrida.toFixed(2)}<br>Valor ótimo para aceitar corrida: R$ ${valorOtimo.toFixed(2)}<br>`;
+    
+    document.getElementById('resultado').innerHTML = resultado;
+});
